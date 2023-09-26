786508f9b3eccdefcec5a34bf6f97dd8dd0fd6a5
s01ver <solverglace@gmail.com>
2023-09-26 19:56:32 +0900

    Initial Commit

---
+++ iosstudy2-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iosstudy2-2
+//
+//  
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
